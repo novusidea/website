7dbc6de7769839a4c05b0c2f245b502875de8a55
Martin Skroch <martin.sk@13-grad.com>
2019-09-11 13:52:14 +0200

    Change dist folder to docs

---
+++ docs/precache-manifest.f43b6be28d27eae3c397581af1d5ac55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "02497c27c70155ffd6d0",
+    "url": "/js/chunk-vendors.e6f7c778.js"
+  },
+  {
+    "revision": "8556b5d589c13e492c03",
+    "url": "/js/app.c9e8343b.js"
+  },
+  {
+    "revision": "7533de03ac8c99f7f059",
+    "url": "/js/about.3ee2f4b8.js"
+  },
+  {
+    "revision": "f9b846f589b164e85f03d3d953cef0c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8556b5d589c13e492c03",
+    "url": "/css/app.ba629074.css"
+  }
+];
