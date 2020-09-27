56dc83c400863b2cf987b34589a7fcddab143bb4
mbehrendts <dev@novusidea.de>
2020-09-27 13:14:54 +0200

    deploy

---
+++ precache-manifest.40a59d5becbf092cfac984c9d4eb89aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c06717e7dac1747f737f9a501f98b2b",
+    "url": "/.htaccess"
+  },
+  {
+    "revision": "30556891ff5345da2dfd",
+    "url": "/css/app.ec870840.css"
+  },
+  {
+    "revision": "339829203df43e0b0b10f9155afc2e20",
+    "url": "/favicon.svg"
+  },
+  {
+    "revision": "b10d158e8910679070f55e2cc64a959b",
+    "url": "/img/logo.b10d158e.svg"
+  },
+  {
+    "revision": "6ab7a0a2e8171f6568a9255763aa8924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30556891ff5345da2dfd",
+    "url": "/js/app.6eb836e3.js"
+  },
+  {
+    "revision": "5f8a184116c93d4aa757",
+    "url": "/js/chunk-vendors.95c06038.js"
+  },
+  {
+    "revision": "e8ba960d17b6b57b8bfe65610bc6552a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
